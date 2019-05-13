6d1ddb8e8da337ffefd908d186bb8bf7861bf6b7
vancong <tavancong1994@gmail.com>
2019-05-13 10:24:54 +0700

    Create TimGTMang.java
    Tim gia tri mang

---
+++ TimGTMang.java
@@ -0,0 +1,24 @@
+package Lession2;
+import java.util.Scanner;
+
+public class TimGTMang {
+    public static void main(String[] args)
+    {
+        String[] students = {"Christina","Michael","Camila", "Sienna", "Tanya", "Connor", "Zachariah", "Mallory", "Zoe", "Emily"};
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a neme's student: ");
+        String input_name = scanner.nextLine();
+
+        boolean isExist = false;
+        for(int i = 0;i< students.length;i++)
+        {
+            if(students[i].equals(input_name)){
+                System.out.println("Position of the students in the list "+input_name+ " is: " +(i+1));
+                isExist = true;
+                break;
+            }
+        }
+        if(!isExist)
+            System.out.println("Not found "+ input_name+" in the list.");
+    }
+}
